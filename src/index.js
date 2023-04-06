4afe7bea5a233bce591b1872f380217371a5a351
abhinav101901 <abhinav8299233838@gmail.com>
2023-04-06 19:16:25 +0530

    food2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose');
 const route = require('./router/router')
 const multer=require("multer")
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 mongoose.set({strictQuery:true})
 
 app.use(express.json())
@@ -17,6 +20,6 @@ mongoose.connect("mongodb+srv://abhinav:abhi123@cluster0.qicwtqo.mongodb.net/FSO
 
 app.use('/', route)
 
-app.listen(3000, function() {
-    console.log('Express app running on port ' +  3000)
+app.listen(3022, function() {
+    console.log('Express app running on port ' +  3022)
 });
